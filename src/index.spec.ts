f4e1ed8dbc5f5ada91f90753af80003399f0ad86
Steve Bosman <steve.bosman@parexel.com>
2018-03-14 23:53:00 +0000

    Add linting; tidy up code

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,46 +1,46 @@
-import { expect } from 'chai';
+import {expect} from 'chai';
 import 'mocha';
-import { isClose } from './index';
+import {isClose} from './index';
 
 describe('isClose function test', () => {
     it('should return true for identical values', () => {
-        let result = isClose(1.0, 1.0);
+        const result = isClose(1.0, 1.0);
         expect(result).to.equal(true);
     });
     it('should return false for very different values', () => {
-        let result = isClose(1.0, 2.0);
+        const result = isClose(1.0, 2.0);
         expect(result).to.equal(false);
     });
     it('should return true for slightly different large values', () => {
-        let result = isClose(1e10, 1.00001e10);
+        const result = isClose(1e10, 1.00001e10);
         expect(result).to.equal(true);
     });
     it('should return false for different small values', () => {
-        let result = isClose(1e-7, 1e-8);
+        const result = isClose(1e-7, 1e-8);
         expect(result).to.equal(false);
     });
     it('should return false for two NaNs', () => {
-        let result = isClose(NaN, NaN);
+        const result = isClose(NaN, NaN);
         expect(result).to.equal(false);
     });
     it('should return true for two NaNs when equalNan is true', () => {
-        let result = isClose(NaN, NaN, undefined, undefined, true);
+        const result = isClose(NaN, NaN, undefined, undefined, true);
         expect(result).to.equal(true);
     });
     it('should return true when absolute tolerance is met', () => {
-        let result = isClose(100000, 100001,0,1);
+        const result = isClose(100000, 100001, 0, 1);
         expect(result).to.equal(true);
     });
     it('should return false when absolute tolerance is breached', () => {
-        let result = isClose(100000, 100001.00000001,0,1);
+        const result = isClose(100000, 100001.00000001, 0, 1);
         expect(result).to.equal(false);
     });
     it('should return true when relative tolerance is met', () => {
-        let result = isClose(1100, 1000,0.1,0);
+        const result = isClose(1100, 1000, 0.1, 0);
         expect(result).to.equal(true);
     });
     it('should return false when relative tolerance is breached', () => {
-        let result = isClose(1100.000001, 1000,0.1,0);
+        const result = isClose(1100.000001, 1000, 0.1, 0);
         expect(result).to.equal(false);
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -7,16 +7,16 @@
  * @param {boolean} equalNan Whether to compare NaN’s as equal.
  *                  If true, when a is NaN and b is NaN they will be considered equal.
  * @returns {boolean} Returns true if both a and b are considered to be close,
- * that is absolute(a - b) <= (atol + rtol * absolute(b)).
+ *                    that is absolute(a - b) <= (atol + rtol * absolute(b)).
  */
 export function isClose(a: number, b: number, rtol = 1e-05, atol = 1e-08, equalNan = false): boolean {
     return (
-               equalNan && isNaN(a) && isNaN(b)
-           )
-           || (
-               a === b
-           )
-           || (
-               Math.abs(a - b) <= atol + rtol * Math.abs(b)
-           );
+            equalNan && isNaN(a) && isNaN(b)
+        )
+        || (
+            a === b
+        )
+        || (
+            Math.abs(a - b) <= atol + rtol * Math.abs(b)
+        );
 }
