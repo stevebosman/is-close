ef28e4f2d82e3d20668eb3802d26491f4bad81ab
Steve Bosman <steve.bosman@parexel.com>
2018-03-19 12:17:00 +0000

    Add ability to use different scaling methods for obtaining relative differences

---
+++ src/isclose.spec.ts
@@ -0,0 +1,130 @@
+import {expect} from 'chai';
+import 'mocha';
+import {isClose, IsCloseScalers} from './index';
+
+describe('isClose function test', () => {
+    it('should return true for identical values', () => {
+        const result = isClose(1.0, 1.0);
+        expect(result).to.equal(true);
+    });
+    it('should return false for two NaNs', () => {
+        const result = isClose(NaN, NaN);
+        expect(result).to.equal(false);
+    });
+    it('should return true for two NaNs when equalNaN is true', () => {
+        const result = isClose(NaN, NaN, undefined, undefined, true);
+        expect(result).to.equal(true);
+    });
+    it('should return true when absolute tolerance is met', () => {
+        const result = isClose(100000, 100001, 0, 1);
+        expect(result).to.equal(true);
+    });
+    it('should return false when absolute tolerance is breached', () => {
+        const result = isClose(100000, 100001.00000001, 0, 1);
+        expect(result).to.equal(false);
+    });
+    it('should return true when one value is 0 and the other -0', () => {
+        const result = isClose(-0, 0);
+        expect(result).to.equal(true);
+    });
+    it('should return true when both values are Infinity', () => {
+        const result = isClose(Infinity, Infinity);
+        expect(result).to.equal(true);
+    });
+    it('should return false when one value is Infinity and the other is -Infinity', () => {
+        const result = isClose(Infinity, -Infinity);
+        expect(result).to.equal(false);
+    });
+    it('should return false when one value is Infinity and the other MAX_VALUE', () => {
+        const result = isClose(Infinity, Number.MAX_VALUE);
+        expect(result).to.equal(false);
+    });
+    it('should return false when one value is Infinity and the other -MAX_VALUE', () => {
+        const result = isClose(-Infinity, -Number.MAX_VALUE);
+        expect(result).to.equal(false);
+    });
+    describe('calculate relative difference using largest of a and b in magnitude - |a-b|/max(|a|,|b|)', () => {
+        it('should return true for slightly different values', () => {
+            const result = isClose(1.0, 1.0000000001);
+            expect(result).to.equal(true);
+        });
+        it('should return false for sufficiently different values', () => {
+            const result = isClose(1.0, 1.000000001);
+            expect(result).to.equal(false);
+        });
+        it('should return true for slightly different large values', () => {
+            const result = isClose(1e10, 1.0000000001e10);
+            expect(result).to.equal(true);
+        });
+        it('should return false for different small values', () => {
+            const result = isClose(1e-7, 1e-8);
+            expect(result).to.equal(false);
+        });
+        it('should return true when relative tolerance is met', () => {
+            const result = isClose(1111.1111111111111, 1000, 0.1, 0);
+            expect(result).to.equal(true);
+        });
+        it('should return false when relative tolerance is breached', () => {
+            const result = isClose(1111.1111111111112, 1000, 0.1, 0);
+            expect(result).to.equal(false);
+        });
+    });
+    describe('calculate relative error using a as theoretical value  - |a-b|/|a|', () => {
+        it('should return true when relative tolerance is met (above)', () => {
+            const result = isClose(1000, 1010, 0.01, 0, undefined, IsCloseScalers.absA);
+            expect(result).to.equal(true);
+        });
+        it('should return false when relative tolerance is breached (above)', () => {
+            const result = isClose(1000, 1010.0000000000001, 0.01, 0, undefined, IsCloseScalers.absA);
+            expect(result).to.equal(false);
+        });
+        it('should return true when relative tolerance is met (below)', () => {
+            const result = isClose(1000, 990, 0.01, 0, undefined, IsCloseScalers.absA);
+            expect(result).to.equal(true);
+        });
+        it('should return false when relative tolerance is breached (below)', () => {
+            const result = isClose(1000, 989.9999999999999, 0.01, 0, undefined, IsCloseScalers.absA);
+            expect(result).to.equal(false);
+        });
+    });
+    describe('calculate relative error using b as theoretical value - |a-b|/|b|', () => {
+        it('should return true when relative tolerance is met (above)', () => {
+            const result = isClose(1010, 1000, 0.01, 0, undefined, IsCloseScalers.absB);
+            expect(result).to.equal(true);
+        });
+        it('should return false when relative tolerance is breached (above)', () => {
+            const result = isClose(1010.0000000000001, 1000, 0.01, 0, undefined, IsCloseScalers.absB);
+            expect(result).to.equal(false);
+        });
+        it('should return true when relative tolerance is met (below)', () => {
+            const result = isClose(990, 1000, 0.01, 0, undefined, IsCloseScalers.absB);
+            expect(result).to.equal(true);
+        });
+        it('should return false when relative tolerance is breached (below)', () => {
+            const result = isClose(989.9999999999999, 1000, 0.01, 0, undefined, IsCloseScalers.absB);
+            expect(result).to.equal(false);
+        });
+    });
+    describe('compute closeness for arrays', () => {
+        it('should return [true, false] when two arrays with appropriate values is supplied', () => {
+            const result = isClose([1, 1], [1.0000000001, 1.000000001]);
+            expect(result.length).to.equal(2);
+            expect(result[0]).to.equal(true);
+            expect(result[1]).to.equal(false);
+        });
+        it('should return [true, false, undefined] when two different sized  with appropriate values is supplied', () => {
+            const result = isClose([1e10, 1e-7, 2], [1.0000000001e10, 1e-8]);
+            expect(result.length).to.equal(3);
+            expect(result[0]).to.equal(true);
+            expect(result[1]).to.equal(false);
+            expect(result[2]).to.equal(undefined);
+        });
+        it('should return [true, false, undefined] when two different sized arrays with appropriate values is supplied', () => {
+            const result = isClose([1e-7, 1e10], [1e-8, 1.0000000001e10, 1]);
+            expect(result.length).to.equal(3);
+            expect(result[0]).to.equal(false);
+            expect(result[1]).to.equal(true);
+            expect(result[2]).to.equal(undefined);
+        });
+    });
+});
